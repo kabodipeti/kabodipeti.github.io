893f72dad95eace0b3df8f20c68fcd69f143fe4f
kabodipeti <kabodipeti@gmail.com>
2021-01-05 23:53:24 +0100

    felt├Âlt├ęs bat file ├íltal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,26 @@
 /* window.setTimeout(function () {
     window.location.reload();
   }, 3000); */
+
+/*   $(document).ready(function() {
+    $('#header_to_include').load('../../header.html');
+}); */
+
+window.onload = function(){
+
+  fetch("../../header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("#header_to_include").innerHTML = data;
+  });
+
+  fetch("../../head.html")
+  .then(response => {
+    return response.text()
+  }) 
+  .then(data => {
+    document.querySelector("#document_head").innerHTML = data;
+  });
+}
