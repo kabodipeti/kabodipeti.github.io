853e96a42b06848f3724d0616b4e61f4529484dc
kabodipeti <38507332+kabodipeti@users.noreply.github.com>
2021-01-07 12:19:14 +0100

    feltöltés

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,88 +5,66 @@
 /*   $(document).ready(function() {
     $('#header_to_include').load('../../header.html');
 }); */
-var d = new Date();
-var start = d.getTime();
+
 var logging = true;
 var header = document.getElementById("header_to_include");
 
 
 
-window.onload =  function(){
-  
-  getHTMLfromFile("../../header.html", "header_to_include");
+$(window).on("load", function(){
+  var d = new Date();
+  var start = d.getTime();
+  getHTMLfromFile("../../header.html", "header_to_include", addEventListeners);
   getHTMLfromFile("../../footer.html", "footer_to_include");
-  
+
   // var mainContextText = getJSON("../../data/proba.json");
   // logger(mainContextText);
-
+  
   // getHTMLfromFile("../../data/proba.json", "peti");
   // logger("peti: " + mainContext.cegnev);
-  document.getElementById("peti").innerText = mainContext.cegnev;
-  var end = d.getTime();
+  removeComments();
+
+  var d2 = new Date();
+  var end = d2.getTime();
   var duration = end - start;
   logger("script lefutott: " + duration + " ms alatt");
-  }
-
-function getFile(fileName){
-  $.getJSON("demo_ajax_json.js", function(result){
-    return result.cegnev;
   });
+
+
+function getHTMLfromFile (htmlPath, targetID, callBack){ 	
+  $("#" +  targetID ).load(htmlPath, callBack);
 }
-function getJSONObject(path){
-  $.getJSON(path, function(data){
-    logger("data12: " + data.cegnev);
-    mainContext = data;
-    logger("data12: " + mainContext.cegnev);
-}).fail(function(){
-    console.log("An error has occurred at getJSONObject.");
-});
+  
+function addEventListeners(){
+  logger("addEventListeners elkezdődöütt");
+  $("#mobile_menu_button").click(function(){
+    toggleMobileMenu();
+  });
+  logger("addEventListeners lefutott");
 }
+
+function toggleMobileMenu(){
+    $(".mobile_menu").slideToggle("fast");
+    logger("mobilos menü toggle");
+}
+
 /* kommentek eltávolítása */
-$('*').contents().each(function() {
-  if(this.nodeType === Node.COMMENT_NODE) {
-    $(this).remove();
-  }
-});
-
-
-function getHTMLfromFileOld (htmlPath, targetID){
-
-  fetch(htmlPath)
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.getElementById(targetID).innerHTML = data;
+function removeComments(){
+  $('*').contents().each(function() {
+    if(this.nodeType === Node.COMMENT_NODE) {
+      $(this).remove();
+    }
   });
-  logger(htmlPath + " betöltve a " + targetID + " ID-jű element-be" );
 }
 
-function getHTMLfromFile (htmlPath, targetID){ 	
-  $("#" +  targetID ).load( htmlPath );
-}
+
+
 
 function changeText(targetID, innerText){
   document.getElementById(targetID).innerText = innerText;
 }
 
-function getJSON(filePath){
-  var xmlhttp = new XMLHttpRequest();
-  var responseText;
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      responseText = this.responseText;
-      var myObj = JSON.parse(responseText);
-      // logger(this.responseText);
-       document.getElementById("telefon").innerHTML = myObj.telefon_szerviz;
-       changeText("ugyfelszolg_elerhetoseg" , myObj.uzletek.baross.nyt_short);
-      return  this.responseText;
-    }
-  };
-  xmlhttp.open("GET", filePath, true);
-  xmlhttp.send();
-  
-}
+
 
 
 function logger(text){
